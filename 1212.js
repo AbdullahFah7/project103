b3984bfa89c1f609ede6813ed796ff7975d66f3b
Abdullah Fahad Alharbi <146173322+AbdullahFah7@users.noreply.github.com>
2023-11-20 15:46:51 +0300

    Update and rename 1212.js to test

--- 1212.js
+++
@@ -1,3 +0,0 @@
-let fruits = ['Apple', 'Pineapple', 'Blueberry', 'Orange'] ;
-    [fruits[0], fruits[1], fruits[2], fruits[3] ] = ['Strawberries', 'Banana', 'Grape', 'Cherry']
-    console.log(fruits) 
